5e7dd514044bf35c524da93826196a8d43516c31
Zucker <ph27868@515372-mitll.llan.ll.mit.edu>
2019-02-01 17:40:33 -0500

    added start of chain to building CSC matrices via hmatrix

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,10 +1,6 @@
-{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE QuasiQuotes, MultiParamTypeClasses #-}
 {-# LANGUAGE TemplateHaskell #-}
-module Lib
-    ( someFunc,
-    	myfun,
-    	demo, testsp
-    ) where
+module Lib where
 
 
 import qualified Language.C.Inline as C
@@ -13,6 +9,8 @@ import qualified Data.Vector.Storable as V
 import qualified Numeric.LinearAlgebra as LA
 import qualified Numeric.LinearAlgebra.Data as D
 import qualified Numeric.LinearAlgebra.Devel as D
+import Control.Arrow ((***))
+import Data.Tuple (swap)
 C.context (C.baseCtx <> C.vecCtx)
 
 C.include "<math.h>"
@@ -22,10 +20,49 @@ C.include "<osqp/osqp.h>"
 testsp = D.mkSparse [((0,0), 3.0), ((1,1), 2.0)]
 
 
+assocVec :: (Num a, Ord a) => [a] -> [(Int,a)] 
+assocVec v = filter (\(_,x) -> x /= 0) (zipWith (,) [0..] v)
+
+-- conveniently, will assume that if your list stops, it is implied the rest of it is zero. Or maybe inconvenitly from a type safety persepctive
+assocMat :: (Num a, Ord a) => [[a]] -> [((Int,Int),a)] 
+assocMat m = let m' = map assocVec m in addRows m' where addRows m'' = concat $ zipWith (\i xs -> map (\(j,x) -> ((i,j),x)) xs) [0 ..] m''
+-- AssocMatrix = [((Int, Int), Double)]
+
+transposeAssoc = map (swap *** id) 
+
+mkGCSC = D.tr . D.mkSparse . transposeAssoc 
+mkGCSC' = mkGCSC . assocMat
+mkCSC :: D.AssocMatrix -> CSC'
+mkCSC = transpose'' . D.mkCSR . transposeAssoc 
+mkCSC' :: [[Double]] -> CSC'
+mkCSC' = mkCSC . assocMat
+-- The CSC type is internal to hmatrix.
+data CSC' = CSC'
+        { cscVals  :: V.Vector Double
+        , cscRows  :: V.Vector CInt
+        , cscCols  :: V.Vector CInt
+        , cscNRows :: Int
+        , cscNCols :: Int
+        } deriving Show
+
+-- data OSQPCSC = CSC  -- {nzmax :: CInt, m :: CInt, n :: CInt, col_p :: V.Vector Ptr, col  }
+{-
+instance LA.Transposable D.CSR CSC'
+  where
+  tr (D.CSR vs cs rs n m) = CSC' vs cs rs m n
+  tr' = tr
+
+instance LA.Transposable CSC' D.CSR
+  where
+  tr (CSC' vs rs cs n m) = D.CSR vs rs cs m n
+  tr' = tr
+-}
+transpose'' (D.CSR vs cs rs n m) = CSC' vs cs rs m n
+transpose''' (CSC' vs rs cs n m) = D.CSR vs rs cs m n
 
 
-main :: IO ()
-main = do
+funboy :: IO ()
+funboy = do
   x <- [C.exp| double{ cos(1) } |]
   print x
 someFunc :: IO ()
@@ -39,10 +76,10 @@ myfun x = [C.pure| double{ cos( $(double x)) } |]
 -- exampleOSQPData = 
 
 -- Actually CDoubles
-data OSQPData = OSQPData {dn :: CInt, dm :: CInt, p :: CSC , 
-     a :: CSC, q :: V.Vector CDouble, l :: V.Vector CDouble, u :: V.Vector CDouble}
+--data OSQPData = OSQPData {dn :: CInt, dm :: CInt, p :: CSC , 
+--     a :: CSC, q :: V.Vector CDouble, l :: V.Vector CDouble, u :: V.Vector CDouble}
 
-data CSC = CSC  -- {nxmax :: CInt, m :: CInt, n :: CInt, col_p :: V.Vector Ptr, col  }
+
 
 
 -- OSQPWorkspace *osqp_setup(const OSQPData *data, OSQPSettings *settings)
@@ -53,6 +90,8 @@ data CSC = CSC  -- {nxmax :: CInt, m :: CInt, n :: CInt, col_p :: V.Vector Ptr, 
 
 -- Maybe the easiest thing is to just inject default settings and marshal only what we need
 -- Get a demo code in full c and start pulling pieces out.
+-- l and u are the easiest
+
 demo :: IO CInt
 demo = [C.block|
 int {
